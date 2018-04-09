d651579e3cd5f8ae16560e69e9afe5e89528897d
Brandon Seamer <brandonseamer@gmail.com>
2018-04-09 13:28:00 -0500

    Enrichment Learning
    Me being assigned additional enrichment work to basic programs.

--- Cartwright Industries/Cartwright Industries/Source.cpp
+++ Cartwright Industries/Cartwright Industries/Source.cpp
@@ -7,13 +7,25 @@ int main()
 {
 
 	//Declare Variables
-	double grossPay;
+	double hoursWorked;
+	double hourlyWage;
 	double FWT =.2;
 	double FICA = .08;
 	double stateTax = .04;
+	double grossPay;
 	//Process
-	std::cout << "Input the employee's weekly gross pay." << std::endl;
-	std::cin >> grossPay;
+	std::cout << "Input the employee's hours worked." << std::endl;
+	std::cin >> hoursWorked;
+	std::cout << "Input the employee's hourly wage." << std::endl;
+	std::cin >> hourlyWage;
+	if (hoursWorked > 40)
+	{
+		grossPay = hourlyWage*40 + (hoursWorked-40)*1.5;
+	}
+	else{
+		grossPay = hourlyWage*hoursWorked;
+	}
+	std::cout << "The employee's gross pay is:" << grossPay << ". " << std::endl;
 	std::cout << "The employee's weekly net pay is:" << grossPay - grossPay*FWT - grossPay*FICA - grossPay*stateTax << ". "<< std::endl;
 	//Conclusion
 	system("pause");
